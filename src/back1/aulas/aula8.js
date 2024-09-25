5dc39d66a3e924649bb433d1b3c860a78347abec
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-09-25 12:58:46 -0300

    estudando

---
+++ src/back1/aulas/aula8.js
@@ -0,0 +1,10 @@
+/*
+
+Uma API (Interface de Programação de Aplicações) é um conjunto de definições e protocolos que permite que diferentes sistemas ou aplicativos sejam comunicados entre si. Elas funcionam como competitivas, permitindo que um software "converse" com outro, trocando dados de forma padronizada
+
+APIs facilitam a integração entre sistemas, como aplicativos de terceiros com bancos de dados ou serviços online. Por exemplo, quando você usa um aplicativo de clima, ele pode usar uma API para buscar dados meteorológicos de um serviço
+
+As APIs podem ser RESTful (mais comuns atualmente), que seguem princípios simples, como comunicação por HTTP usando verbos como GET, POST, PUT, DELETE, ou SOAP, que são mais complexos
+
+Eles ajudam a modularizar sistemas, tornar o desenvolvimento mais rápido e eficiente, e promover a interoperabilidade entre diversas tecnologias.
+*/
