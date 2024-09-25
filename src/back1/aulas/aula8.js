cdd242c22b847e2fafc25777e121c597473a1031
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-09-25 17:02:53 -0300

    mais estudo

--- src/back1/aulas/aula8.js
+++ src/back1/aulas/aula8.js
@@ -1,5 +1,7 @@
 /*
 
+    - API'S
+
 Uma API (Interface de Programação de Aplicações) é um conjunto de definições e protocolos que permite que diferentes sistemas ou aplicativos sejam comunicados entre si. Elas funcionam como competitivas, permitindo que um software "converse" com outro, trocando dados de forma padronizada
 
 APIs facilitam a integração entre sistemas, como aplicativos de terceiros com bancos de dados ou serviços online. Por exemplo, quando você usa um aplicativo de clima, ele pode usar uma API para buscar dados meteorológicos de um serviço
@@ -7,4 +9,57 @@ APIs facilitam a integração entre sistemas, como aplicativos de terceiros com 
 As APIs podem ser RESTful (mais comuns atualmente), que seguem princípios simples, como comunicação por HTTP usando verbos como GET, POST, PUT, DELETE, ou SOAP, que são mais complexos
 
 Eles ajudam a modularizar sistemas, tornar o desenvolvimento mais rápido e eficiente, e promover a interoperabilidade entre diversas tecnologias.
+
+API PRIVADA:
+A API é usada apenas internamente. Isso oferece às empresas um maior controle.
+
+Elas são restritas a um determinado conjunto de pessoas que possuem
+credenciais de autenticação ou autorização para acessar a API.
+
+Uma API privada é geralmente usada para proteger dados confidenciais ou
+restringir o acesso a recursos específicos
+
+API PUBLICA:
+A API é disponibilizada para todos, sem restrições de acesso ou autenticação.
+
+Terceiros podem desenvolver aplicações que interajam com a sua API e isso pode
+se tornar uma fonte de inovação.
+
+As APIs públicas geralmente são protegidas por limites de uso, como cotas de
+solicitação de API ou autenticação de chave de API para evitar o uso excessivo
+ou indevido da API.
+
+
+    ENDPOINTS
+
+Endpoints
+
+Um endpoint de uma API é um ponto final, ou seja, um endereço de URL específico
+que representa um recurso ou serviço disponibilizado por essa API.
+
+Quando um cliente (por exemplo, um aplicativo, site ou sistema) faz uma solicitação
+HTTP para um endpoint de API, ele recebe uma resposta do servidor contendo as
+informações solicitadas.
+
+    ● Em outras palavras, um endpoint é um canal de comunicação que permite que
+    um cliente se conecte a um serviço ou recurso específico fornecido por uma API.
+
+Cada endpoint é projetado para executar uma ação específica, como recuperar dados
+de um banco de dados, executar uma operação de negócios ou retornar informações
+de um sistema externo.
+
+Os endpoints de API geralmente são especificados em uma documentação da
+API, que detalha a sintaxe da solicitação HTTP necessária para acessar cada
+endpoint, bem como os parâmetros e opções de resposta que a API suporta.
+
+Alguns exemplos de endpoints podem incluir
+● /clientes para recuperar informações do cliente
+● /produtos para retornar informações sobre um produto específico
+● /pedidos para obter informações sobre pedidos em um sistema de comércio
+eletrônico.
+
+Em resumo, os endpoints de uma API são URLs que representam os recursos ou
+serviços disponibilizados por essa API, e são usados pelos clientes para acessar
+e interagir com esses recursos ou serviços.
+
 */
