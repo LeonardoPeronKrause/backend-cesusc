315f12ff2d5e7957062e8e9a13078abeb1aa5c33
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-09-25 10:39:41 -0300

    nova estutura

--- src/back1/aulas/aula6.js
+++ src/back1/aulas/aula6.js
@@ -20,3 +20,16 @@ const n4 = 10;
 media = (n1 + n2 + n3 + n4) / 4;
 
 console.log(media);
+
+// ESTRUTURA -> FOR
+for (iniciação; condição; incremento) {
+    //bloco de código a ser executado
+}
+
+// let i = 0; // Exemplo de inicialização
+// i < 10; // O loop continua enquanto i for menor que 10
+// i++; // Incrementa i em 1 a cada iteração
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
