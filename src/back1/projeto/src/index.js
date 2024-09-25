f4889d10ee84ac263e5bff5762a10b0e21024aa3
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-09-25 12:25:45 -0300

    estudando

---
+++ src/back1/projeto/src/index.js
@@ -0,0 +1,32 @@
+//const fs = require('fs');
+//fs.writeFileSync('file.txt', 'Hi there...');
+
+// import chalk from 'chalk'; // serve p mudar a cor
+// console.log(chalk.blue('Hello World!'));
+
+// import moment from 'moment'; // serve p ver o moment exato (horario)
+// console.log(moment().format());
+
+//import inquirer from 'inquirer'; // serve p interagir com o user
+/*inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Qual é o seu nome?',
+        },
+    ])
+    .then((answer) => {
+        console.log(`Olá, ${answer.name}`);
+    });
+    */
+ /*
+import dotenv from 'dotenv';
+
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
+    
+// Acessar variáveis de ambiente
+console.log(`Host: ${process.env.DB_HOST}`);
+console.log(`User: ${process.env.DB_USER}`);
+console.log(`Password: ${process.env.DB_PASS}`);*/
