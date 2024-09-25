385ea5853e27623b926b64fa3f9daa8c28594ea9
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-09-24 21:55:42 -0300

    exercicios basicos com node.js

---
+++ src/back1/aula6.js
@@ -0,0 +1,22 @@
+// Exercícios
+// 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+console.log('Alo mundo');
+
+// 2. Faça um Programa que converta metros para centímetros.
+
+const metro = 10;
+const cm = metro * 100;
+
+console.log(`${metro} metro(s) é igual a ${cm}cm`);
+
+// 3. Faça um Programa que pegue 4 notas adicionadas em variáveis e mostre a média.
+
+const n1 = 10;
+const n2 = 5;
+const n3 = 5;
+const n4 = 10;
+
+media = (n1 + n2 + n3 + n4) / 4;
+
+console.log(media);
